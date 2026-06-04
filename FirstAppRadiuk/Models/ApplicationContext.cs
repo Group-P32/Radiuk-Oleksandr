4a95bb28b18791df80fb77934a5b70d678260434
Radiuk Oleksandr <rodiona07ua@gmail.com>
2026-06-04 19:35:17 +0200

    Практична 12 - ASP.NET Identity

---
+++ FirstAppRadiuk/Models/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FirstAppRadiuk.Models
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext() : base("IdentityDb") { }
+
+        public static ApplicationContext Create()
+        {
+            return new ApplicationContext();
+        }
+    }
+}
---
+++ FirstAppRadiuk/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FirstAppRadiuk.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int Year { get; set; }
+    }
+}
